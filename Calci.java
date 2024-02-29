4be88f4877762cf779e6d440b917b80683d59e71
narasimhakatti456 <narasimhakatti456@gmail.com>
2024-02-29 14:40:20 +0530

    Added

--- Calci.java
+++ Calci.java
@@ -1,6 +1,9 @@
 interface Calci{
-void int getSum(int n1,int n2);
+    void int getSum(int n1,int n2);
 }
-
-
-
+    class AddCalsi implements Calci{
+    public int getSum(int n1, int n2) {
+        int ans = n1+n2;
+        return ans;
+    }
+}
