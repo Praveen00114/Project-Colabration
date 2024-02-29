cc637e709245e8ab09ab14d2dd0eb0b9569ae98e
Yeswanth8618 <yeswanth9696@gmail.com>
2024-02-29 14:33:33 +0530

    updated

--- Calci.java
+++ Calci.java
@@ -1,5 +1,10 @@
 interface Calci{
-void int getSum(int n1,int n2);
+ int getSum(int n1,int n2);
+}
+class calcimpl implements Calci{
+   public int getSum(int n1,int n2){
+        return n1/n2;
+    }
 }
 
 
