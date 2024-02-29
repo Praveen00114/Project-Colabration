c6e6540875522c682fe6ba92c944eb60fd3fa111
sayabanna4128 <sabukarakal2020@gmail.com>
2024-02-29 14:31:33 +0530

    Guys i have provided impl class for Calsi interface to substraction

--- Calci.java
+++ Calci.java
@@ -1,6 +1,12 @@
 interface Calci{
 void int getSum(int n1,int n2);
 }
+class SubCalsi implements Calci{
+    public int getSum(int n1, int n2) {
+        int res = n1-n2;
+        return res;
+    };
+}
 
 
 
